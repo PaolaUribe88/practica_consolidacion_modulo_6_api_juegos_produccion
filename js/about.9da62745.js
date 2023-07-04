df11e795d37e01c71d49627ada31a0c1db9d7024
PaolaUribe88 <paolauribe88@gmail.com>
2023-07-03 22:50:52 -0400

    Repositorio Local, proyecto M6

---
+++ js/about.9da62745.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkpractica_consolidacion_modulo_6_api_juegos"]=self["webpackChunkpractica_consolidacion_modulo_6_api_juegos"]||[]).push([[443],{782:function(a,n,o){o.r(n),o.d(n,{default:function(){return l}});var c=o(252);const u={class:"about"},i=(0,c._)("h1",null,"This is an about page",-1),s=[i];function t(a,n){return(0,c.wg)(),(0,c.iD)("div",u,s)}var e=o(744);const r={},_=(0,e.Z)(r,[["render",t]]);var l=_}}]);
+//# sourceMappingURL=about.9da62745.js.map
